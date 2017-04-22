72cc70ab79e07a855693f96f7f0fdf89cd199a90
RyanErnst <rernst1@umbc.edu>
2017-04-22 13:04:46 -0400

    working connection to oracle.jdbc.OracleDriver

--- CMSC461_Project5.java
+++ CMSC461_Project5.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
+import java.sql.*;
 
 public class CMSC461_Project5{
    public static void main(String []args){
       System.out.println("Hello World");   
+      JDBC("testID", "Test1234");
+   }
+   
+   public static void JDBC(String userID, String password){
+      try{
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         System.out.println("USER_ID = " + userID + ", PSW = " + password);
+      }
+      catch(Exception sqle){
+         System.out.println("Exception: " + sqle);
+      }
    }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CMSC461_Project5
 This is a repository for the Spring 2017, CMSC 461, Project5
-As of this iteration, a blank JAVA file is committed.
+As of this iteration, a method for running the SQL queries and connecting the the database 
+has been entered.
